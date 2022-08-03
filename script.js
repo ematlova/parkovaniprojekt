8f6d58a973c949b3ce7552f4bc2d5a1b3600a821
Ester Mátlová <ester.matlova@gmail.com>
2022-08-03 13:47:14 +0200

    script

---
+++ script.js
@@ -0,0 +1,27 @@
+client = new Paho.MQTT.Client("d57a0d1c39d54550b147b58411d86743.s2.eu.hivemq.cloud", 8884, "letni-skola" + Math.random());
+client.connect({
+    onSuccess: onConnect,
+    userName: "robot", 
+    password: "P@ssW0rd!",
+    useSSL: true
+});
+
+function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+
+    client.onMessageArrived = onMessageArrived;
+    client.subscribe("/out/parking/button/enter");
+    client.subscribe("/out/parking/button/exit");
+}
+
+function onMessageArrived(message) {
+
+    if (message.destinationName == "/out/parking/button/enter") {
+        document.getElementById("/out/parking/button/enter").innerText = message.payloadString
+    }
+    else {
+        document.getElementById("/out/parking/button/exit").innerText = message.payloadString
+    }
+
+}
